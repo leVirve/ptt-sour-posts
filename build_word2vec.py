5e7786b16aec1271480d114581f2c3e267e5f718
Salas leVirve <gae.m.project@gmail.com>
2017-06-05 06:41:37 +0100

    build up full piepline

--- build_word2vec.py
+++ build_word2vec.py
@@ -1,27 +1,29 @@
+import click
 import jieba
 from pymongo import MongoClient
-from gensim.models import word2vec
+from gensim import models
 
 
 class Word2VecModel():
 
     name = 'output/%s-%d.model.bin'
 
-    def __init__(self, filename, corpus_file, encode_size=250):
+    def __init__(self, filename):
         self.filename = filename
-        self.corpus_file = corpus_file
-        self.encode_size = encode_size
 
-    def train(self):
-        sentences = word2vec.Text8Corpus(self.corpus_file)
-        model = word2vec.Word2Vec(sentences, size=self.encode_size, workers=8)
-        model.save(self.name % (self.filename, self.encode_size))
+    def train(self, corpus_file, encode_size=250):
+        sentences = models.word2vec.Text8Corpus(corpus_file)
+        model = models.word2vec.Word2Vec(sentences, size=encode_size, workers=8)
+        model.save(self.name % (self.filename, encode_size))
+
+    def load(self, encode_size=250):
+        return models.Word2Vec.load(self.name % (self.filename, encode_size))
 
 
 class Corpus():
 
     def __init__(self, filename):
-        self.writer = open(corpus_output, 'w', encoding='utf8')
+        self.writer = open(filename, 'w', encoding='utf8')
 
     def write_words(self, words):
         self.writer.write(' '.join(words) + ' ')
@@ -63,21 +65,36 @@ class PttDatabase():
         return self.collection.count()
 
 
-if __name__ == '__main__':
-
-    board = 'baseball'
-    corpus_file = f'output/corpus_{board}.txt'
-
+def create_corpus(board, corpus_file):
     db = PttDatabase(board)
-
     corpus = Corpus(corpus_file)
     tokenizer = Tokenizer()
-    for i, post in enumerate(db.posts()):
+    for i, post in enumerate(db.posts(), start=1):
         corpus.write_words(tokenizer.segment(post['content']))
         for comment in post['comments']:
             corpus.write_words(tokenizer.segment(comment['content']))
         print(f'{board}, 處理第 {i} 篇文章，共 %d 篇' % len(db))
     corpus.close()
 
-    model = Word2VecModel(board, corpus_file, encode_size=250)
-    model.train()
+
+@click.command()
+@click.option('--board', default='baseball', help='選擇資料來源看板')
+@click.option('--train', is_flag=True, help='是否訓練 word2vec')
+@click.option('--encode_size', default=250, help='word2vec 向量長度')
+@click.option('--text', default='', help='測試字串')
+def main(board, train, encode_size, text):
+    corpus_file = f'output/corpus_{board}.txt'
+    w2v = Word2VecModel(board)
+
+    if train:
+        create_corpus(board, corpus_file)
+        w2v.train(corpus_file, encode_size=encode_size)
+
+    model = w2v.load(encode_size=encode_size)
+    res = model.most_similar(text.strip(), topn=20)
+    vec = model[text]
+    print(vec, '\n', res)
+
+
+if __name__ == '__main__':
+    main()
